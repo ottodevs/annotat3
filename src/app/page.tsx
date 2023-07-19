267d314f1f4e14a450c155581439bf87b06972ee
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-19 14:28:46 +0200

    fix homepage route and div within p error

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+
+type Props = {};
+
+const HomePage: FC<Props> = () => {
+    return (
+        <>
+           <h1>Home Page</h1>
+        </>
+    );
+}
+
+export default HomePage;
