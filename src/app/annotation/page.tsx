9f08c31589fff5478e72dd4dfb2a60ae6a7f0d9a
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-19 09:06:48 +0200

    minor changes to pages structure

--- src/app/annotation/page.tsx
+++ src/app/annotation/page.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link'
 export default function Annotation() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-start p-24 min-h-full">
       Annotation Page
+      <Link href="/home">Home</Link>
+      <Link href="/statistics">Statistics</Link>
+      <Link href="/library">Library</Link>
+      <Link href="/profile">Profile</Link>
     </main>
   )
 }
