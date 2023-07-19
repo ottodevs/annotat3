74f73b40fa54143000e912627bf8c5a7d8d4941b
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-19 10:27:04 +0200

    add basic ceramic authentication and test queries (#9)

--- src/app/annotation/page.tsx
+++ src/app/annotation/page.tsx
@@ -45,7 +45,7 @@ export default function Annotation() {
   return (
     <main className="flex flex-col items-center justify-start p-24 min-h-full">
       Annotation Page
-      <Link href="/home">Home</Link>
+      <Link href="/">Home</Link>
       <Link href="/statistics">Statistics</Link>
       <Link href="/library">Library</Link>
       <Link href="/profile">Profile</Link>
