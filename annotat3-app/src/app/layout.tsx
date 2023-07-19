cebe5c73ffb0c904509e09c5bfe4d341346a8ff3
Lyons800 <oisinlyons13@gmail.com>
2023-07-19 09:24:04 +0200

    initial

--- annotat3-app/src/app/layout.tsx
+++ annotat3-app/src/app/layout.tsx
@@ -8,6 +8,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Sidebar } from "@/components/sidebar.component"
 import { Profile } from '@/types'
+import SidebarProvider from '@/providers/SidebarProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,10 +28,37 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <CeramicContextProvider>
+          <SidebarProvider>
+          <section className="flex h-full w-full">
+                    <Sidebar />
+
+                    {/* Navbar & Main Content */}
+                    <div className="h-full w-full bg-lightPrimary dark:!bg-navy-900">
+
+                        {/* Main Content */}
+                        <main className='mx-[12px] h-full flex-none transition-all md:pr-2 xl:ml-[313px]' >
+                            {/* Routes */}
+                            <div className="h-full">
+                                {/* <Navbar /> */}
+
+                                <div className="pt-5s mx-auto mb-auto h-full min-h-[84vh] p-2 md:pr-2">
+                                    {children}
+                                </div>
+
+                                <div className="p-3">
+                                    {/* <Footer /> */}
+                                </div>
+                            </div>
+                        </main>
+
+                    </div>
+
+                </section>
+          </SidebarProvider>
         <Sidebar name = {profile?.name} username = {profile?.username} id={profile?.id}/>
         {children}
         </CeramicContextProvider>
-        
+
         </body>
     </html>
   )
